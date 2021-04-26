4a7bafef1f237e82bf42aa820fc9e8c0958291e5
gidachmad <gaaf0809@gmail.com>
2021-04-26 13:06:00 +0700

    commit produklist

---
+++ produklist.php
@@ -0,0 +1,52 @@
+<?php
+require_once "view/header.php";
+?>
+
+  <div class="grid produk-list main-container">
+    <div class="produk-item card flex">
+      <img src="dist/img/coffee-mugs-1727056_1920.jpg" alt="">
+      <h2> Nama Produk </h2>
+      <p>Deskripsi Singkat. Lorem ipsum dolor sit, amet consectetur adipisicing elit.!</p>
+      <p>Harga : Rp. 15.000</p>
+      <a href="" class="link">Lihat Produk</a>
+    </div>
+    <div class="produk-item card flex">
+      <img src="dist/img/coffee-mugs-1727056_1920.jpg" alt="">
+      <h2> Nama Produk </h2>
+      <p>Deskripsi Singkat. Lorem ipsum dolor sit, amet consectetur adipisicing elit.!</p>
+      <p>Harga : Rp. 15.000</p>
+      <a href="" class="link">Lihat Produk</a>
+    </div>
+    <div class="produk-item card flex">
+      <img src="dist/img/coffee-mugs-1727056_1920.jpg" alt="">
+      <h2> Nama Produk </h2>
+      <p>Deskripsi Singkat. Lorem ipsum dolor sit, amet consectetur adipisicing elit.!</p>
+      <p>Harga : Rp. 15.000</p>
+      <a href="" class="link">Lihat Produk</a>
+    </div>
+    <div class="produk-item card flex">
+      <img src="dist/img/coffee-mugs-1727056_1920.jpg" alt="">
+      <h2> Nama Produk </h2>
+      <p>Deskripsi Singkat. Lorem ipsum dolor sit, amet consectetur adipisicing elit.!</p>
+      <p>Harga : Rp. 15.000</p>
+      <a href="" class="link">Lihat Produk</a>
+    </div>
+    <div class="produk-item card flex">
+      <img src="dist/img/coffee-mugs-1727056_1920.jpg" alt="">
+      <h2> Nama Produk </h2>
+      <p>Deskripsi Singkat. Lorem ipsum dolor sit, amet consectetur adipisicing elit.!</p>
+      <p>Harga : Rp. 15.000</p>
+      <a href="" class="link">Lihat Produk</a>
+    </div>
+    <div class="produk-item card flex">
+      <img src="dist/img/coffee-mugs-1727056_1920.jpg" alt="">
+      <h2> Nama Produk </h2>
+      <p>Deskripsi Singkat. Lorem ipsum dolor sit, amet consectetur adipisicing elit.!</p>
+      <p>Harga : Rp. 15.000</p>
+      <a href="" class="link">Lihat Produk</a>
+    </div>
+  </div>
+
+<?php
+require_once "view/footer.php";
+?>
