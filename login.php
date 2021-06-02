ed3fcb08478f1d9babecdb5638472d13aecb0dff
Gid AAF <gaaf0809@gmail.com>
2021-06-02 08:26:47 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ require_once "view/header.php";
 <div class="login flex">
   <h1 class="title"> LOG IN</h1>
   <form action="" class="flex form">
-      <!-- isi value ku nu geus aya dina database, meh mun teu jadi ngedit bisa langsung pencet konfirm atau update -->
+      <!-- jang login, mun bisa pang nambahkeun validasi na -->
     <input class="input" type="text" name="nama" placeholder="Nama" required>
     <input class="input" type="password" name="pass" placeholder="Password" required>
 
