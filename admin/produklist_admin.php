119cfdad0edd5e43d82d645e78def053cb42384b
Khoirul Fahmi <khoirulfahmi44@gmail.com>
2021-06-10 12:54:06 +0700

    tambah fungsi jika produklist kosong

--- admin/produklist_admin.php
+++ admin/produklist_admin.php
@@ -22,24 +22,28 @@ if (!isset($_SESSION["login"])) {
     <?php
     $query = "SELECT * FROM produk ORDER BY idproduk ASC";
     $result = mysqli_query($conn, $query);
-    while ($row = mysqli_fetch_assoc($result)) {
+    if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
     ?>
-      <div class="produk-item card flex">
-        <img class="thumb" src="../dist/img/gambar_produk/<?php echo $row['gambar']; ?>" alt="">
-        <h2> <?php echo $row['namaproduk']; ?> </h2>
-        <p><?php echo substr($row['deskripsi'], 0, 70) ?>...</p>
-        <h3><?php echo rupiah($row['harga']); ?></h3>
-        <a href="produk_admin.php?idproduk=<?php echo $row['idproduk']; ?>" class=" link">Lihat Produk</a>
-        <form action="" class="flex form">
-          <!-- ieu di hidden meh langsung bisa ngadelete datana, -->
-          <input class="input" type="text" name="id_produk" value="" hidden>
-          <!-- ieu link ngedit na langsung per produk, hiji hiji -->
-          <a href="produk_edit.php?idproduk=<?php echo $row['idproduk']; ?>" class="link"> Edit </a>
-          <a href="proses_hapus_produk.php?idproduk=<?php echo $row['idproduk']; ?>" class="link" onclick="return confirm('Anda yakin akan menghapus data ini?')"> Hapus </a>
-        </form>
-      </div>
-    <?php
-    } ?>
+        <div class="produk-item card flex">
+          <img class="thumb" src="../dist/img/gambar_produk/<?php echo $row['gambar']; ?>" alt="">
+          <h2> <?php echo $row['namaproduk']; ?> </h2>
+          <p><?php echo substr($row['deskripsi'], 0, 70) ?>...</p>
+          <h3><?php echo rupiah($row['harga']); ?></h3>
+          <a href="produk_admin.php?idproduk=<?php echo $row['idproduk']; ?>" class=" link">Lihat Produk</a>
+          <form action="" class="flex form">
+            <!-- ieu di hidden meh langsung bisa ngadelete datana, -->
+            <input class="input" type="text" name="id_produk" value="" hidden>
+            <!-- ieu link ngedit na langsung per produk, hiji hiji -->
+            <a href="produk_edit.php?idproduk=<?php echo $row['idproduk']; ?>" class="link"> Edit </a>
+            <a href="proses_hapus_produk.php?idproduk=<?php echo $row['idproduk']; ?>" class="link" onclick="return confirm('Anda yakin akan menghapus data ini?')"> Hapus </a>
+          </form>
+        </div>
+      <?php
+      }
+    } else { ?>
+      <h2 align="center">Tidak Ada Produk</h2>
+    <?php } ?>
 
   </div>
 </div>
--- produklist.php
+++ produklist.php
@@ -7,17 +7,21 @@ include 'script/koneksi.php';
   <?php
   $query = "SELECT * FROM produk ORDER BY idproduk ASC";
   $result = mysqli_query($conn, $query);
-  while ($row = mysqli_fetch_assoc($result)) {
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
   ?>
-    <div class="produk-item card flex">
-      <img class="thumb" src="dist/img/gambar_produk/<?php echo $row['gambar']; ?>" alt="">
-      <h2> <?php echo $row['namaproduk']; ?> </h2>
-      <p> <?php echo $row['komposisi']; ?> </p>
-      <p><?php echo substr($row['deskripsi'], 0, 70) ?>...</p>
-      <h3><?php echo rupiah($row['harga']); ?></h3>
-      <a href="produk.php?idproduk=<?php echo $row['idproduk']; ?>" class="link">Lihat Produk</a>
-    </div>
-  <?php  } ?>
+      <div class="produk-item card flex">
+        <img class="thumb" src="dist/img/gambar_produk/<?php echo $row['gambar']; ?>" alt="">
+        <h2> <?php echo $row['namaproduk']; ?> </h2>
+        <p> <?php echo $row['komposisi']; ?> </p>
+        <p><?php echo substr($row['deskripsi'], 0, 70) ?>...</p>
+        <h3><?php echo rupiah($row['harga']); ?></h3>
+        <a href="produk.php?idproduk=<?php echo $row['idproduk']; ?>" class="link">Lihat Produk</a>
+      </div>
+    <?php  }
+  } else { ?>
+    <h2 align="center">Tidak ada Produk</h2>
+  <?php } ?>
 </div>
 
 <?php
